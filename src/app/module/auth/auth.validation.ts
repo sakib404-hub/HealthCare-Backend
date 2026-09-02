fc79def6f640395fa03278f1cc45db76a4aae41a
sakib404-hub <akibhossainsakib1040@gmail.com>
2026-09-02 17:35:17 +0600

    sanitization and validation in separate file

---
+++ src/app/module/auth/auth.validation.ts
@@ -0,0 +1,23 @@
+import z from "zod";
+
+const PatientRegistratationZodSchema = z.object({
+	name: z.string("Not a string"),
+	email: z.string(),
+	password: z
+		.string()
+		.min(8, "Password must be at least 8 character")
+		.max(30, "Password can't be more than 30 character")
+		.regex(/[A-Z]/, "Password must contain at least one Uppercase.")
+		.regex(/[a-z]/, "Password must contain at least one Lowercase.")
+		.regex(/[0-9]/, "Password must contain at least one number")
+		.regex(/[^A-Za-z0-9]/, "Password must contain at least one special character."),
+	patient: z
+		.object({
+			contactNumber: z.string().optional(),
+		})
+		.optional(),
+});
+
+export const UserValidation = {
+    PatientRegistratationZodSchema
+}
