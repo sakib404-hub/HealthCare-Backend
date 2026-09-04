267ffd55a90c9bf71ffd5ff37bfd340f728f6ca9
sakib404-hub <akibhossainsakib1040@gmail.com>
2026-09-04 22:27:00 +0600

    uploading image in cloudinary successfull

--- src/app/module/user/user.controller.ts
+++ src/app/module/user/user.controller.ts
@@ -2,16 +2,23 @@ import type{ NextFunction, Request, Response } from "express";
 import status from "http-status";
 import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
+import { UserServices } from "./user.service";
 
 const uploadProfileImage = catchAsync(async(req : Request, res : Response, next : NextFunction)=>{
 
-    console.log(req.file, "This is the req.file")
+    if(!req.file){
+        throw new Error("No File Provided.");
+    }
+
+    const buffer = req.file?.buffer;
+
+    const result = await UserServices.uploadProfileImage(buffer as Buffer)
 
     return sendResponse(res, {
         statusCode : status.OK,
         success : true,
         message : "",
-        data : null
+        data : result
     })
 })
 
--- src/app/module/user/user.service.ts
+++ src/app/module/user/user.service.ts
@@ -1,5 +1,15 @@
-const uploadProfileImage = async()=>{
+import cloudinary from "../../lib/cloudinary"
 
+const uploadProfileImage = async(buffer : Buffer)=>{
+    const cloudinaryResult =  cloudinary.uploader.upload_stream({resource_type : "auto"}, (error, result)=>{
+        if(error){
+            console.log(error.message);
+            throw new Error(error.message);
+        }
+
+        console.log(result?.secure_url);
+        console.log(result);
+    }).end(buffer)
 }
 
 export const UserServices = {
